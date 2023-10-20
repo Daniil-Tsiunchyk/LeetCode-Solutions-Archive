d065e9a090ce8389dc606c979810771014db46a7
Neural-Enigma <fcad.td@gmail.com>
2023-10-20 16:05:15 +0300

    Solved problem №13

---
+++ LeetCode/Task_13/Main.java
@@ -0,0 +1,7 @@
+package Task_13;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MCMXCIV = " + Solution.romanToInt("MCMXCIV") + " = " + 1994);
+    }
+}
