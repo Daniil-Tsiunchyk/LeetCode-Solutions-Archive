e1376e8060efc38e861618d617e84cc0c692f7ea
Neural-Enigma <fcad.td@gmail.com>
2023-10-21 16:46:22 +0300

    Solved problem №12

---
+++ LeetCode/Task_12/Main.java
@@ -0,0 +1,7 @@
+package Task_12;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1994 = " + Task_12.Solution.intToRoman(1994) + " = MCMXCIV");
+    }
+}
