e3d4844b64c537c175728c2d397da68c7ef5f7c3
Neural-Enigma <fcad.td@gmail.com>
2023-10-23 04:03:35 +0300

    Solved problem №342

---
+++ LeetCode/Task_342/Solution.java
@@ -0,0 +1,16 @@
+package Task_342;
+
+public class Solution {
+
+    public static boolean isPowerOfFour(int n) {
+        if (n <= 0) {
+            return false;
+        }
+
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+
+        return n == 1;
+    }
+}
