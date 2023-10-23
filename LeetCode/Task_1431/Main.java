6f1e729049c9f995706290f548b12d2a5a0dd981
Daniil-Tsiunchyk <fcad.td@gmail.com>
2023-10-23 23:42:35 +0300

    Solved problem №1431
    LeetCode 75

---
+++ LeetCode/Task_1431/Main.java
@@ -0,0 +1,9 @@
+package Task_1431;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3));
+        System.out.println(Solution.kidsWithCandies(new int[]{4, 2, 1, 1, 2}, 1));
+        System.out.println(Solution.kidsWithCandies(new int[]{12, 1, 12}, 10));
+    }
+}
