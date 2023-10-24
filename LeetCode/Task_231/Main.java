df103807d62bdb83e2ed09cb3dbbdb63bd4b9f5c
Daniil-Tsiunchyk <fcad.td@gmail.com>
2023-10-24 03:39:19 +0300

    Solved problems №231 and №326

---
+++ LeetCode/Task_231/Main.java
@@ -0,0 +1,9 @@
+package Task_231;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.isPowerOfTwo(1));
+        System.out.println(Solution.isPowerOfTwo(16));
+        System.out.println(Solution.isPowerOfTwo(3));
+    }
+}
---
+++ LeetCode/Task_326/Main.java
@@ -0,0 +1,9 @@
+package Task_326;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.isPowerOfThree(27));
+        System.out.println(Solution.isPowerOfThree(0));
+        System.out.println(Solution.isPowerOfThree(-1));
+    }
+}
