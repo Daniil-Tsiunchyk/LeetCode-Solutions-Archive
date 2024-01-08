9930edfc49ace482494ce1dd040559a9e3ea3e9c
Daniil-Tsiunchyk <fcad.td@gmail.com>
2024-01-08 04:52:11 +0300

    feat: solved problem №938

---
+++ LeetCode/Task_938/Solution.java
@@ -0,0 +1,17 @@
+package Task_938;
+
+class Solution {
+    public static int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        } else if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+    }
+}
